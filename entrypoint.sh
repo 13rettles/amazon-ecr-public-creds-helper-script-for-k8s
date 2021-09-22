83d0d03432306a0bdfcfac76dfe9f6773a34de27
Brett Walker <brwalker@tableau.com>
2021-09-22 13:34:42 -0700

    Entrypoint fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
+#!/usr/bin/env bash
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
-
-#!/usr/bin/env bash
 set -euo pipefail
 
 REGISTRY=https://public.ecr.aws
@@ -20,6 +19,6 @@ do
         --docker-password=${AUTH_TOKEN}
     # Uncomment the following lines if you want to make all pods use this auth token without specifying it in each PodSpec.
     #kubectl patch serviceaccount default \
-    #    -n "${n}"
+    #    -n "${n}" \
     #    -p '{"imagePullSecrets":[{"name":"ecr-public-token"}]}'
 done
